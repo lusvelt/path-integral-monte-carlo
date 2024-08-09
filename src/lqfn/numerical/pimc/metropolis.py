fa89c5d8f50b9f8823e138733f5124e5fe726563
Federico Tonetto <federico.tonetto@studio.unibo.it>
2024-08-09 14:08:06 +0200

    implement bootstrap procedure for errors

--- src/lqfn/numerical/pimc/metropolis.py
+++ src/lqfn/numerical/pimc/metropolis.py
@@ -28,7 +28,7 @@ def compute_path_integral_average(functional: Callable, S_per_component: Callabl
         N_cf (int): Total number of samples contributing to be saved during the process for computing the path integral average.
         N_cor (int): Number of path updates before picking each sample.
         eps (float): $\\epsilon$ parameter for the update of the path.
-        thermalization_its (int): Number of samples to be discarded at the beginning to let the procedure thermalize. Default is 10.
+        thermalization_its (int): Number of samples to be discarded at the beginning to let the procedure thermalize. Default is 5.
 
     Returns:
         numpy.ndarray[float, N]: Path integral average of the functional.
@@ -48,3 +48,43 @@ def compute_path_integral_average(functional: Callable, S_per_component: Callabl
     avg = functional_samples.mean(axis=0)
     std = functional_samples.std(axis=0) / np.sqrt(N_cf)
     return avg, std
+
+def compute_path_integral_average_bootstrap (functional: Callable, S_per_component:Callable ,N: int, N_cf: int, N_cor: int, eps: float, thermalization_its: int=5): 
+    """
+    Computes the path integral average and the standard deviation of a functional obtained using the bootstrap procedure.
+
+    Args:
+        functional(Callable[numpy.ndarray[float, N], float]): A functional taking a path as input (as a numpy array of length N) and returning a number.
+        S_per_component(Callable[int, numpy.ndarray[float, N]]): A functional taking as input an integer $j$ and a path, and returning the contribution of the $j$-th component of each path point to the action.
+        N (int): Number of path points.
+        N_cf (int): Total number of samples contributing to be saved during the process for computing the path integral average.
+        N_cor (int): Number of path updates before picking each sample.
+        eps (float): $\\epsilon$ parameter for the update of the path.
+        thermalization_its (int): Number of samples to be discarded at the beginning to let the procedure thermalize. Default is 5.
+
+    Returns:
+        numpy.ndarray[float, N]: Path integral average of the functional.
+        numpy.ndarray[float, N]: Standard deviation error associated to each instant of time in the path integral average.
+
+    """
+    functional_samples = np.zeros((N_cf, N))                                      
+    path = np.zeros(N)
+    for j in range(N): 
+        path[j] = 0                               
+    for j in range(thermalization_its*N_cor):   # thermalization
+        _update_path(path, S_per_component, eps)
+    for rows in range(N_cf):                     
+        for j in range(N_cor):   # discard N_cor values
+            _update_path(path, S_per_component, eps)
+        for n in range(N):                    
+            functional_samples[rows][n] = functional(path, n)
+    matrix_of_functionals_bootstrap = np.zeros((N_cf,N))
+    for rows in range (N_cf):
+        index_of_copied_path = int(np.random.uniform(0,N_cf))
+        for n in range (N):   
+            matrix_of_functionals_bootstrap[rows][n] = functional_samples[index_of_copied_path][n] 
+    avg = matrix_of_functionals_bootstrap.mean(axis=0)
+    std = matrix_of_functionals_bootstrap.std(axis=0) /np.sqrt(N_cf)
+    return avg,std
+
+
